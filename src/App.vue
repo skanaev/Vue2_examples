242ad9db9d3fa199d2ce56efab4a932079e55b88
Sergey Kanaev <skanaev1993@gmail.com>
2021-09-30 19:21:27 +0300

    add some examples for vue2

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/meetup">Frontend Meetup</router-link>
     </div>
     <router-view/>
   </div>
@@ -10,11 +10,16 @@
 
 <style lang="less">
 #app {
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  margin: 0px !important;
 }
 
 #nav {
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,73 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const getDefaultState = () => {
+  return {
+    user: null,
+    employees: []
+  }
+}
+
+const fetchData = (ms) => {
+  return new Promise((resolve, reject) => {
+  if (ms > 100000000) {reject('слишком долго ждать')}
+  setTimeout(() => {
+    resolve({name: 'Ivan', age: 30, profession: 'manager'})
+  }, ms)
+})
+}
+
+const fetchEmployees = (ms) => {
+  return new Promise((resolve, reject) => {
+  if (ms > 100000000) {reject('слишком долго ждать')}
+  setTimeout(() => {
+    resolve(
+      [
+        {name: 'Anna', age: 18, profession: 'hr'},
+        {name: 'Olga', age: 18, profession: 'coder'},
+        {name: 'Sergey', age: 22, profession: 'coder'},
+        {name: 'Andrey', age: 25, profession: 'coder'},
+        {name: 'Pavel', age: 30, profession: 'coder'}
+      ]
+    )
+  }, ms)
+})
+}
+
 export default new Vuex.Store({
   state: {
+    user: null,
+    employees: []
+  },
+  
+  actions: {
+    reset (context) {
+      context.commit('resetState')
+    },
+    getUserInfo ({commit}) {
+      fetchData(3000).then(result => commit('updateUser', result))
+                     .catch(error => console.log(error))
+    },
+    getEmployees ({commit}) {
+      fetchEmployees(5000).then(result => commit('updateEmployees', result))
+      .catch(error => console.log(error))
+    }
   },
   mutations: {
+    resetState () {
+      Object.assign(state, getDefaultState())
+    },
+    updateUser (state, payload) {
+      state.user = payload
+    },
+    updateEmployees (state, payload) {
+      state.employees = payload
+    }
   },
-  actions: {
+  getters: {
+    onlyCoders (state) {
+      return state.employees.filter (e => e.profession === 'coder')
+    }
   },
   modules: {
   }
